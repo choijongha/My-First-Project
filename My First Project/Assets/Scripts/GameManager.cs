370a7de615fb6351c01fd2d3aef52714f69e0e28
choijongha <80575094+choijongha@users.noreply.github.com>
2022-01-21 20:47:53 +0900

    더블 클릭 구현
    더블 클릭시 카메라 확대
    UI 카메라 위치 수정바람

--- My First Project/Assets/Scripts/GameManager.cs
+++ My First Project/Assets/Scripts/GameManager.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 using TMPro;
 public class GameManager : MonoBehaviour
 {
-    public bool selected;
+    public bool onSelected;
+    public bool onOneClicked;
+    public bool onDoubleClicked;
+    public float timerForDoubleClick;
+    public float delay = 0.5f;
+
     public GameObject seletedRingPrefabs;
     public GameObject mainCamera;
     public GameObject nameBox;
@@ -13,6 +18,7 @@ public class GameManager : MonoBehaviour
 
     private Vector2 targetPosition;
     private RaycastHit2D hit;
+
     void Start()
     {
         rectTransform = nameBox.GetComponent<RectTransform>();
@@ -22,6 +28,7 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         Selected();
+        DoubleClickFalse();
     }
     void Selected()
     {
@@ -30,31 +37,34 @@ public class GameManager : MonoBehaviour
             Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
-            BoolOnSelected();
+            BoolOnonSelected();
+
         }
     }
     
-    void BoolOnSelected()
+    void BoolOnonSelected()
     {
         if (hit.collider != null)
         {
             //Vector2 targetPosition = GameObject.Find($"{hit.collider.name}").transform.position;
             targetPosition = hit.transform.position;
             UIName();
-            if (!selected && !GameObject.FindGameObjectWithTag("SelectedRing"))
+            DoubleClick();
+            if (!onSelected && !GameObject.FindGameObjectWithTag("SelectedRing"))
             {
-                selected = true;
+                onSelected = true;
                 nameBox.SetActive(true);
                 Instantiate(seletedRingPrefabs, targetPosition, transform.rotation);
             }
-            else if (selected && GameObject.FindGameObjectWithTag("SelectedRing"))
+            else if (onSelected && GameObject.FindGameObjectWithTag("SelectedRing"))
             {
                 GameObject.FindGameObjectWithTag("SelectedRing").transform.position = targetPosition;
+                if (Time.time - timerForDoubleClick > delay) ;
             }
         }
         else
         {
-            selected = false;
+            onSelected = false;
             Destroy(GameObject.FindGameObjectWithTag("SelectedRing"));
             nameBox.SetActive(false);
         }
@@ -64,4 +74,31 @@ public class GameManager : MonoBehaviour
         nameText.text = hit.collider.name;
         rectTransform.position = Camera.main.WorldToScreenPoint(targetPosition) - new Vector3(0, 30);
     }
+    void DoubleClick()
+    {
+        if (!onOneClicked)
+        {
+            onOneClicked = true;
+            timerForDoubleClick = Time.time;
+        }
+        else
+        {
+            onOneClicked = false;
+            onDoubleClicked = true;
+            Debug.Log("Double Click");
+            mainCamera.GetComponent<Camera>().orthographicSize = 1f;
+        }
+    }
+    void DoubleClickFalse()
+    {
+        if (onOneClicked)
+        {
+            if (Time.time - timerForDoubleClick > delay)
+            {
+                onOneClicked = false;
+                Debug.Log("onOneClicked out");
+            }
+        }
+    }
 }
+
